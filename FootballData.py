8b43b3806a9daa1fcfc3452ee0567c7f2ad982f3
bw20 <67421809+bw20@users.noreply.github.com>
2020-11-22 18:03:13 +1100

    Update FootballData.py

--- FootballData.py
+++ FootballData.py
@@ -9,7 +9,7 @@ class FootballData(object):
     def create_df(self):
         def seasons_dictionary(starting_decade, ending_decade):
             #Create a dictionary with the decades and seasons for each dataset.
-            eng = 'https://github.com/bw20/football-dashboard/tree/main/england-master'
+            eng = 'https://raw.githubusercontent.com/bw20/football-dashboard/main/england-master'
             seasons = {}
             for i in range(int(abs((starting_decade - ending_decade)/10)) + 1):
                 decade = starting_decade
